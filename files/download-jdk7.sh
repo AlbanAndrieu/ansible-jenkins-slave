28142547988b3de87738a61d4a9c006dd637afe3
jenkins <alban.andrieu@free.fr>
2014-11-28 16:04:22 +0100

    Activate JDK7

--- files/download-jdk7.sh
+++ files/download-jdk7.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # {{ ansible_managed }}
 
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7/$1 -O $1
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/$1 -O $1
