b1976808dd7699ae17edfb76a84a29af9fd04449
Alban Andrieu <alban.andrieu@free.fr>
2020-08-27 10:58:35 +0200

    Update

--- files/database/oracle/step-0-0-install.sh
+++ files/database/oracle/step-0-0-install.sh
@@ -9,6 +9,7 @@ echo -e "${yellow} ${bold}INSTALLING ORACLE INSTANT CLIENT ${NC}"
 
 #HOME="/home/jenkins" on linux
 #HOME="/Users/jenkins" on osx
+DOWNLOAD_URL="https://albandrieu.com/download"
 
 if [[ ! -d /opt/oracle/instantclient_12_2/help/ ]]; then
   echo "${green} Installing oracle/instantclient ${NC}"
@@ -16,39 +17,37 @@ if [[ ! -d /opt/oracle/instantclient_12_2/help/ ]]; then
     mkdir -p /opt/oracle
     cd /opt/oracle
     #cd ${HOME}/Developer
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-basic-solaris.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-basic-solaris.x64-12.2.0.1.0.zip
     unzip instantclient-basic-solaris.x64-12.2.0.1.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-jdbc-solaris.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-jdbc-solaris.x64-12.2.0.1.0.zip
     unzip instantclient-jdbc-solaris.x64-12.2.0.1.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-odbc-solaris.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-odbc-solaris.x64-12.2.0.1.0.zip
     unzip instantclient-odbc-solaris.x64-12.2.0.1.0.zip
     ln -s /opt/oracle/instantclient_12_2 /jenkins/Developer/instantclient_12_2
     #LD_LIBRARY_PATH=${HOME}/Developer/instantclient_12_2:$LD_LIBRARY_PATH
   elif [[ "$(uname -s)" == "Darwin" ]]; then
-    #See http://albandri.nabla.mobi/download/oracle/
     mkdir -p /opt/oracle
     cd /opt/oracle
     #cd ${HOME}/Developer
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-basic-macos.x64-12.1.0.2.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-basic-macos.x64-12.1.0.2.0.zip
     unzip instantclient-basic-macos.x64-12.1.0.2.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-jdbc-macos.x64-12.1.0.2.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-jdbc-macos.x64-12.1.0.2.0.zip
     unzip instantclient-jdbc-macos.x64-12.1.0.2.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-odbc-macos.x64-12.1.0.2.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-odbc-macos.x64-12.1.0.2.0.zip
     unzip instantclient-odbc-macos.x64-12.1.0.2.0.zip
     #ln -s /opt/oracle/instantclient_12_2 /Users/jenkins/Developer/instantclient_12_2
     #LD_LIBRARY_PATH=/Users/jenkins/Developer/instantclient_12_1:$LD_LIBRARY_PATH
   elif [[ "$(uname -s)" == "Linux" ]]; then
-    #See http://albandri.nabla.mobi/download/oracle/
     mkdir -p /opt/oracle
     cd /opt/oracle
     #cd ${HOME}/Developer
-    wget --no-check-certificate http://albandri.nabla.mobi/download/oracle/instantclient-basic-linux.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-basic-linux.x64-12.2.0.1.0.zip
     unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-jdbc-linux.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-jdbc-linux.x64-12.2.0.1.0.zip
     unzip instantclient-jdbc-linux.x64-12.2.0.1.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-odbc-linux.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-odbc-linux.x64-12.2.0.1.0.zip
     unzip instantclient-odbc-linux.x64-12.2.0.1.0.zip
-    wget --no-check-certificate https://albandri.nabla.mobi/download/oracle/instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
+    wget --no-check-certificate ${DOWNLOAD_URL}/oracle/instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
     unzip instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
     #ln -s /opt/oracle/instantclient_12_2 /jenkins/Developer/instantclient_12_2
     #mkdir -p /home/jenkins/Developer
@@ -68,10 +67,10 @@ odbcinst -j
 
 #From Unix Client
 echo "Check odbc connection"
-echo "osql -S FR1CSWFRBM0005_2016 -U 'sa' -P 'Almonde01'"
-echo "isql -v FR1CSWFRBM0005_2016 'sa' 'Almonde01'"
-echo "/opt/oracle/instantclient_12_2/sqlplus sys/Almonde01\$@//localhost:1521/ORCLPDB1 as sysdba"
+echo "osql -S TEST_2016 -U 'sa' -P 'microsoft'"
+echo "isql -v TEST_2016 'sa' 'microsoft'"
+echo "/opt/oracle/instantclient_12_2/sqlplus sys/microsoft\$@//localhost:1521/ORCLPDB1 as sysdba"
 #From Windows Client
-echo "sqlcmd -S FR1CSWFRBM0005 -U so -P Almonde01"
+echo "sqlcmd -S TEST -U so -P microsoft"
 
 exit 0
